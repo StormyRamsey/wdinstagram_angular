f476c402152fa8c79c8c3d7b259ba119b02b5da9
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-09 14:23:31 -0400

    added index/show

--- js/app.js
+++ js/app.js
@@ -4,4 +4,33 @@
   angular.module("wdinstagram", ["ui.router"])
 })
 
-();
+.config([
+  "stateProvider",
+  RouterFunction
+])
+
+.controller("WdinstagramIndexController", [
+  WdinstagramIndexControllerFunction
+])
+
+function RouterFunction($stateProvider)
+  $stateProvider
+  .state("wdinstagramIndex", {
+    url:"/wdinstagrams",
+    templateUrl: "js/ng-views/index.html",
+    controller: "WdinstagramIndexController",
+    controllerAs: "vm"
+  })
+
+  .state("wdinstagramShow",{
+    url: "/wdinstagrams/:id",
+    templateUrl: "js/ng-views/show.html"
+  })
+
+function WdinstagramIndexControllerFunction() {
+  this.wdinstagrams = [
+
+  ]
+}
+
+// reference lesson: https://github.com/ga-wdi-lessons/angular-resources
